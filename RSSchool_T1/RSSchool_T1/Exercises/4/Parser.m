af80fe06cf149bcc58625d2b55b128282e7de1be
Artem Bernatsky <bernatskyhome@gmail.com>
2020-03-15 23:10:16 +0300

    feat: add solution for last task

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,35 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
+
+    
+    NSMutableArray<NSString*>* result = [[NSMutableArray alloc] initWithArray:@[]];
+    NSString* openSymbols = @"(<[";
+    NSString* closeSymbols = @")>]";
+    int count = 0;
+
+    for (int i = 0; i < [string length]; i++){
+        for(int j = 0; j < [openSymbols length]; j++){
+            if ([string characterAtIndex: i] == [openSymbols characterAtIndex: j]) {
+                for (int k = i + 1; k < [string length]; k++) {
+                    if ([string characterAtIndex: k] == [openSymbols characterAtIndex: j]) {
+                        count++;
+                        continue;
+                    }
+                    if ([string characterAtIndex: k] == [closeSymbols characterAtIndex: j] && count != 0) {
+                        count--;
+                        continue;
+                    }
+                    if ([string characterAtIndex: k] == [closeSymbols characterAtIndex: j] && count == 0) {
+                        [result addObject:[string substringWithRange:NSMakeRange(i + 1, k - i - 1)]];
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    return result;
 }
 
 @end
